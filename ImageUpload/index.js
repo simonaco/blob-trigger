938df770678458b3df7a59775fcd894b6d88c934
simonaco-circle <simona_cotin@yahoo.com>
2017-11-15 08:10:42 +0000

    Change message

--- ImageUpload/index.js
+++ ImageUpload/index.js
@@ -1,4 +1,10 @@
-module.exports = function (context, myBlob) {
-    context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
-    context.done();
+module.exports = function(context, myBlob) {
+  context.log(
+    'This is working \n Name:',
+    context.bindingData.name,
+    '\n Blob Size:',
+    myBlob.length,
+    'Bytes'
+  );
+  context.done();
 };
